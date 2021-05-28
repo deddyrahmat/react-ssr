59a1dc4ff6f1b92d4c210a489886bd848f380b72
deddyrahmat <deddyrahmat97@gmail.com>
2021-05-28 09:42:28 +0700

    prepare deploy in heroku

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ import express from 'express';
 
 import App from '../src/App';
 
-const PORT = 8080
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 const router = express.Router();
